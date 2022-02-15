82883a7b57dcfd3e831383c56a907d6ec65d4c63
szabinakov <szabinkov@gmail.com>
2022-02-15 15:26:39 +0000

    changed module-require to exportdefault-import

--- datasamp.js
+++ datasamp.js
@@ -1,4 +1,4 @@
 const medications = ["Amoxicillin", "Codeine", "Diclofenac", "Ibuprofen", "Paracetamol", "Simvastatin", "Tramadol", "Warfarin"]
 
 
-module.exports = medications
+export default medications
